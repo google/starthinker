ee5d1b1087d17fc52f52a1e47c1786a2e71a0ca2
Paul Kenjora <kenjora@kenjora-macbookair.roam.corp.google.com>
2019-08-21 21:07:07 -0700

    Fixing account setup.

--- starthinker_ui/account/management/commands/account_setup.py
+++ starthinker_ui/account/management/commands/account_setup.py
@@ -41,7 +41,7 @@ class Command(BaseCommand):
     parser.add_argument(
       '--write',
       action='store_true',
-      dest='verbose',
+      dest='write',
       default=False,
       help='Safety check for a dangerous operation.',
     )
