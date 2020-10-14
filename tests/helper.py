a6cd304cc5b7371f4f539cb839759f89c83cf6b7
Paul Kenjora <kenjora@google.com>
2020-10-13 17:56:35 -0700

    Modify test helper to rely on default test ID value in test JSON, set to 'Manual'.
    PiperOrigin-RevId: 336994217

--- tests/helper.py
+++ tests/helper.py
@@ -15,26 +15,44 @@
 #  limitations under the License.
 #
 ###########################################################################
-"""Command line to run all tests or list all runnable tests.
 
-Meant to speed up automated testing of StarThinker.
 
-To configure: python tests/helper.py --configure
-To run all: python tests/helper.py
-To run some: python tests/helper.py --tests dt entity
+"""Command line to set up and run all the necessary test files for Starthinker.
 
+  The test helper creates and checks a starthinker_assets/tests.json harness
+  file with all the variables required to run each test.  It then runs each test
+  in a process and polls for completion.  Tests run in parallel.
+
+  Examples:
+    To configure: python tests/helper.py --configure
+    To run all: python tests/helper.py
+    To run some: python tests/helper.py --tests dt entity
+
+  Args:
+    -c', --configure: Configure test in starthinker_assets/tests.json only. No run.
+    -t', --tests: Run only these tests, name of test from scripts.
+    -s', --skips: Skip these tests, name of test from scripts.
+    -i', --include: Used for namespacing test files and avoiding collisions.
+    -r', --test_run_id: Specify a test run ID to inject into the test config fields.
+
+  Returns:
+    Writes data to tests/logs/ as [FAILED][OK]_test_name.log.
+    The files contain all STDOUT and STDERR output from each task.
 """
 
-import os
-import re
-import glob
 import argparse
-import subprocess
+import glob
 import json
+import os
+import subprocess
+import re
 from time import sleep
 
-from starthinker.config import UI_ROOT, UI_SERVICE, UI_PROJECT
-from starthinker.script.parse import json_get_fields, json_set_fields
+from starthinker.config import UI_PROJECT
+from starthinker.config import UI_ROOT
+from starthinker.config import UI_SERVICE
+from starthinker.script.parse import json_get_fields
+from starthinker.script.parse import json_set_fields
 from starthinker.util.project import get_project
 
 CONFIG_FILE = UI_ROOT + '/starthinker_assets/tests.json'
@@ -72,13 +90,24 @@ def load_tests():
         yield filename, get_project(TEST_DIRECTORY + filename)
 
 
-def configure_tests(scripts, tests, skips, test_run_id = ''):
-  """Initialize all the necessary test files for Starthinker
+def configure_tests(tests, runs, skips, test_run_id):
+  """Initialize the starthinker_assets/tests.json variable harness.
 
-  Args: None
+  Read all existing tests from tests/scripts/*.json and create a dictionary of
+  each script and fields.  Save that dictionary to a test harness file where
+  developer can configure tests.
+
+  Then read the test harness and create recipe files to that can be run.  Write
+  those files to tests/recipes/*.json for execution in a later step.
+
+  Args:
+    test: List of (filename, json) pairs containing all the tests.
+    runs: List of test names that will be run, all will run if blank.
+    skips: List of tests to skip.
+    test_run_id: String added as a field to each test, used for namespacing.
 
   Returns:
-    None
+    List of JSON recpies, where all fields have values from the test harness.
 
   """
 
@@ -87,18 +116,19 @@ def configure_tests(scripts, tests, skips, test_run_id = ''):
   print('UPDATE CONFIG')
 
   old_fields = {}
-  if (os.path.exists(CONFIG_FILE)):
+  if os.path.exists(CONFIG_FILE):
     with open(CONFIG_FILE, 'r') as f:
       old_fields = json.load(f)
 
   # Get new fields from test files and merge in old values
 
   fields = {}
-  for filename, script in scripts:
+  for filename, script in tests:
     script_fields = json_get_fields(script)
     script_name = filename.split('.')[0]
 
     for field in script_fields:
+      if field['name'] == 'test_run_id': continue
       fields.setdefault(script_name, {})
       fields[script_name][field['name']] = old_fields.get(script_name, {}).get(
           field['name'], field.get('default', ''))
@@ -116,11 +146,12 @@ def configure_tests(scripts, tests, skips, test_run_id = ''):
     f.write(json.dumps(fields, sort_keys=True, indent=2))
     f.close()
 
-    # Inject the test run ID to the list of field values that were read from the
-    # test config file. This is done in memory only, so that concrete test run
-    # value are replaced every time a test runs.
-    for script in fields:
-      fields[script]['test_run_id'] = test_run_id
+    if test_run_id:
+      # Inject the test run ID to the list of field values that were read from the
+      # test config file. This is done in memory only, so that concrete test run
+      # value are replaced every time a test runs.
+      for script in fields:
+        fields[script]['test_run_id'] = test_run_id
   else:
     print('WARNING CONFIGURATION IS EMPTY, CHECK YOUR PATHS!')
 
@@ -129,12 +160,12 @@ def configure_tests(scripts, tests, skips, test_run_id = ''):
   print('GENERATE RECIPES')
   os.makedirs(RECIPE_DIRECTORY, exist_ok=True)
 
-  # Create recipes from scripts
+  # Create recipes from tests
 
   recipes = []
-  for filename, script in scripts:
+  for filename, script in tests:
     name = filename.split('.')[0]
-    if tests and name not in tests:
+    if runs and name not in runs:
       continue
     if name in skips:
       continue
@@ -177,9 +208,29 @@ def configure_tests(scripts, tests, skips, test_run_id = ''):
 
   return recipes
 
-def run_tests(scripts, recipes, tests, skips):
+def run_tests(tests, recipes, runs, skips):
+  """Run tests derived from tests/scripts/*.json.
 
-  if not tests:
+  Each test was written to a file in tests/recipes/*.json, which will spin
+  up a process allowing parallel execution. Each process is monitored for exit
+  status.
+
+  If status is OK, all tasks OK, writes to tests/logs/OK_*.json.
+  If status is ERROR, any task ERROR, writes to tests/logs/OK_*.json.
+
+  Args:
+    test: List of (filename, json) pairs containing test definitions.
+    recipes: List of (filename, json) pairs containing executable recipes.
+    runs: List of test names that will be run, all will run if blank.
+    skips: List of tests to skip.
+
+  Returns:
+    Writes data to tests/logs/ as [FAILED][OK]_test_name.log.
+    The files contain all STDOUT and STDERR output from each task.
+
+  """
+
+  if not runs:
     print('CLEAR LOGS')
     for f in glob.glob(LOG_DIRECTORY + '*.log'):
       os.remove(f)
@@ -188,11 +239,11 @@ def run_tests(scripts, recipes, tests, skips):
 
   jobs = []
   for recipe in recipes:
-    if tests and recipe.split('.')[0] not in tests: continue
+    if runs and recipe.split('.')[0] not in runs: continue
     if recipe.split('.')[0] in skips: continue
 
     command = [
-        '%s/starthinker_virtualenv/bin/python' % UI_ROOT,
+        'python',
         '-W',
         'ignore',
         '%s/starthinker/all/run.py' % UI_ROOT,
@@ -275,11 +326,35 @@ def generate_include(script_file):
 
 def tests():
   parser = argparse.ArgumentParser()
-  parser.add_argument('-c', '--configure', help='Configure test in starthinker_assets/tests.json only.', action='store_true')
-  parser.add_argument('-t', '--tests', nargs='*', help='Run only these tests, name of test from scripts.')
-  parser.add_argument('-s', '--skips', nargs='*', help='Skip these tests, name of test from scripts.')
-  parser.add_argument('-i', '--include', help='Create an include file for the script, used in tests.', default=None)
-  parser.add_argument('-r', '--test_run_id', help='Specify a test run ID to inject into the test config fields.')
+  parser.add_argument(
+    '-c',
+    '--configure',
+    help='Configure test in starthinker_assets/tests.json only.', action='store_true'
+  )
+  parser.add_argument(
+    '-t',
+    '--tests',
+    nargs='*',
+    help='Run only these tests, name of test from tests/scripts/*.json.'
+  )
+  parser.add_argument(
+    '-s',
+    '--skips',
+    nargs='*',
+    help='Skip these tests, name of test from tests/scripts/*.json.'
+  )
+  parser.add_argument(
+    '-i',
+    '--include',
+    default=None,
+    help='Used for namespacing test files and avoiding collisions.'
+  )
+  parser.add_argument(
+    '-r',
+    '--test_run_id',
+    default='Manual',
+    help='Specify a test run ID to inject into the test config fields.'
+  )
 
   args = parser.parse_args()
 
@@ -289,18 +364,15 @@ def tests():
     generate_include(args.include)
 
   else:
-    scripts = list(load_tests())
-    tests = [t.split('.')[0] for t in (args.tests or [])]
+    tests = list(load_tests())
+    runs = [t.split('.')[0] for t in (args.tests or [])]
     skips = [t.split('.')[0] for t in (args.skips or [])]
-    test_run_id = ''
-    if args.test_run_id:
-      test_run_id = '_' + args.test_run_id
 
     if args.configure:
-      configure_tests(scripts, tests, skips, test_run_id)
+      configure_tests(tests, runs, skips, args.test_run_id)
     else:
-      recipes = configure_tests(scripts, tests, skips, test_run_id)
-      run_tests(scripts, recipes, tests, skips)
+      recipes = configure_tests(tests, runs, skips, args.test_run_id)
+      run_tests(tests, recipes, runs, skips)
 
 
 if __name__ == '__main__':
