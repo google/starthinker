7738298e2fc1471c26b970ef5cc2aea467ba82af
Paul Kenjora <pkenjora@gmail.com>
2020-12-10 10:05:55 -0800

    Revert "Update placement.py"

--- starthinker/task/traffic/placement.py
+++ starthinker/task/traffic/placement.py
@@ -360,11 +360,7 @@ class PlacementDAO(BaseDAO):
       result['compatibility'] = 'IN_STREAM_VIDEO'
       result['size'] = {'width': '0', 'height': '0'}
       result['tagFormats'] = ['PLACEMENT_TAG_INSTREAM_VIDEO_PREFETCH']
-   elif feed_item[FieldMap.PLACEMENT_TYPE] == 'IN_STREAM_AUDIO':
-      result['compatibility'] = 'IN_STREAM_AUDIO'
-      result['size'] = {'width': '0', 'height': '0'}
-      result['tagFormats'] = ['PLACEMENT_TAG_INSTREAM_VIDEO_PREFETCH']
-   else:
+    else:
       result['compatibility'] = 'DISPLAY'
       width = 1
       height = 1
