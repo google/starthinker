6516274d408fd865c7aa02c91bbbdc239226375a
Paul Kenjora <kenjora@google.com>
2021-09-23 14:17:24 -0700

    Simplify generated colab template.
    PiperOrigin-RevId: 398575454

--- starthinker/tool/colab.py
+++ starthinker/tool/colab.py
@@ -84,57 +84,66 @@ def recipe_to_colab(name, description, instructions, tasks, parameters={}, proje
   '''))
 
   colab.header('1. Install Dependencies')
-  colab.paragraph(
-      'First install the libraries needed to execute recipes, this only needs to be done once, then click play.'
-  )
+  colab.paragraph('First install the libraries needed to execute recipes, this only needs to be done once, then click play.')
   colab.code('!pip install git+https://github.com/google/starthinker')
 
-  colab.header('2. Get Cloud Project ID')
-  colab.paragraph(
-      'To run this recipe [requires a Google Cloud Project](https://github.com/google/starthinker/blob/master/tutorials/cloud_project.md), this only needs to be done once, then click play.'
-  )
-  #colab.image('Client Project ID', 'https://github.com/google/starthinker/raw/master/tutorials/images/cloud_project.png')
-  colab.code('CLOUD_PROJECT = \'%s\'' % (project or 'PASTE PROJECT ID HERE'))
-  colab.code('\nprint("Cloud Project Set To: %s" % CLOUD_PROJECT)')
+  colab.header('2. Set Configuration')
+  colab.paragraph(textwrap.dedent('''
+    This code is required to initialize the project. Fill in required fields and press play.
 
-  colab.header('3. Get Client Credentials')
+    1. If the recipe uses a Google Cloud Project:
+      - Set the configuration **project** value to the project identifier from [these instructions](https://github.com/google/starthinker/blob/master/tutorials/cloud_project.md).
+
+    1. If the recipe has **auth** set to **user**:
+      - If you have user credentials:
+        - Set the configuration **user** value with your user credentials JSON.
+        - Set the configuration **client** value to None.
+      - If you DO NOT have user credentials:
+        - Set the configuration **client** value to [downloaded client credentials](https://github.com/google/starthinker/blob/master/tutorials/cloud_client_installed.md).
+
+    1. If the recipe has **auth** set to **service**:
+      - Set the configuration **service** value to [downloaded service credentials](https://github.com/google/starthinker/blob/master/tutorials/cloud_service.md).
+
+    1. Otherwise leave all values as they are or set to None.
+  '''))
+
+  #colab.image('Client Project ID', 'https://github.com/google/starthinker/raw/master/tutorials/images/cloud_project.png')
   #colab.image('Client Credentials', 'https://github.com/google/starthinker/raw/master/tutorials/images/cloud_client_installed.png')
-  colab.paragraph(
-      'To read and write to various endpoints requires [downloading client credentials](https://github.com/google/starthinker/blob/master/tutorials/cloud_client_installed.md), this only needs to be done once, then click play.'
-  )
-  colab.code('CLIENT_CREDENTIALS = \'%s\'' % (client_credentials or 'PASTE CLIENT CREDENTIALS HERE'))
-  colab.code('\nprint("Client Credentials Set To: %s" % CLIENT_CREDENTIALS)')
+
+  colab.code('from starthinker.util.configuration import Configuration')
+  colab.code('')
+  colab.code(textwrap.dedent('''
+    CONFIG = Configuration(
+      project="PASTE PROJECT ID HERE",
+      client="IF USING AUTH user IN RECIPE, PASTE CLIENT CREDENTIALS JSON OR PATH HERE",
+      service="IF USING AUTH service IN RECIPE, PASTE SERVICE CREDENTIALS HERE",
+      user="/content/user.json",
+      verbose=True
+    )
+  '''))
 
   fields = json_get_fields(tasks)
   if fields:
-    colab.header('4. Enter %s Parameters' % name)
+    colab.header('3. Enter %s Recipe Parameters' % name)
     colab.list(instructions)
-    colab.paragraph(
-        'Modify the values below for your use case, can be done multiple times, then click play.'
-    )
+    colab.paragraph('Modify the values below for your use case, can be done multiple times, then click play.')
     colab.code('FIELDS = %s' % fields_to_string(fields, parameters))
     colab.code('\nprint("Parameters Set To: %s" % FIELDS)')
 
-  colab.header('%d. Execute %s' % (5 if fields else 4, name))
-  colab.paragraph(
-      'This does NOT need to be modified unless you are changing the recipe, click play.'
-  )
+  colab.header('%d. Execute %s' % (4 if fields else 3, name))
+  colab.paragraph('This does NOT need to be modified unless you are changing the recipe, click play.')
 
-  colab.code('from starthinker.util.configuration import Configuration')
   colab.code('from starthinker.util.configuration import execute')
   colab.code('from starthinker.util.recipe import json_set_fields')
   colab.code('')
-  colab.code("USER_CREDENTIALS = '/content/user.json'")
-  colab.code('')
-  colab.code('TASKS = %s' %
-             dict_to_string(json_set_auths(tasks, 'user'), skip=('field',)))
+  colab.code('TASKS = %s' % dict_to_string(json_set_auths(tasks, 'user'), skip=('field',)))
   colab.code('')
 
   if fields:
     colab.code('json_set_fields(TASKS, FIELDS)')
   colab.code('')
 
-  colab.code('execute(Configuration(project=CLOUD_PROJECT, client=CLIENT_CREDENTIALS, user=USER_CREDENTIALS, verbose=True), TASKS, force=True)')
+  colab.code('execute(CONFIG, TASKS, force=True)')
 
   return colab.render()
 
